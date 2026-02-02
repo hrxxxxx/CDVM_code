3ae3ced154f45853e8405e2fb636632fc61857ff
hrxxxxx <369874425@qq.com>
2026-02-02 16:54:56 +0800

    Update test_cross_class.sh

--- test_cross_class.sh
+++ test_cross_class.sh
@@ -1,1 +1,1 @@
-python test_cross_class.py --model vit_base_patch16_224_8k_vocab --data_path /disk/yxc/datasets/mvtec_anomaly_detection
+python test_cross_class.py --model vit_base_patch16_224_8k_vocab --data_path /mvtec_anomaly_detection
